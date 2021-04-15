321e36ed37992e388beab60425142070d1705551
Ricardo Ariel <ricardoacs89@gmail.com>
2021-04-14 21:23:58 -0300

    Projeto base criado

---
+++ src/application/Program.java
@@ -0,0 +1,57 @@
+package application;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JOptionPane;
+
+import entities.Inover;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		List<Inover> lista = new ArrayList<>();
+		boolean sucesso = new File("C:\\relatorioFelicidadeInovers").mkdir();
+		
+		String caminhoAquivo = "C:\\relatorioFelicidadeInovers\\relatorio.csv";
+		int novo = 0;
+		
+		while (novo == 0){
+			
+			String nome = JOptionPane.showInputDialog(null, "Qual o nome do Inover?");
+						
+			int pergunta1 = Integer.parseInt(JOptionPane.showInputDialog(null, "De 1 a 5, qual seu grau de felicidade com o trabalho em equipe?"));
+			int pergunta2 = Integer.parseInt(JOptionPane.showInputDialog(null, "De 1 a 5, qual seu grau de felicidade com a sua liderança?"));
+			int pergunta3 = Integer.parseInt(JOptionPane.showInputDialog(null, "De 1 a 5, qual seu grau de felicidade com o prazo do projeto?"));
+			int pergunta4 = Integer.parseInt(JOptionPane.showInputDialog(null, "De 1 a 5, qual seu grau de felicidade com o seu desempenho?"));
+			int pergunta5 = Integer.parseInt(JOptionPane.showInputDialog(null, "De 1 a 5, como você avalia a sua felicidade com a InoveJr?"));
+						
+			lista.add(new Inover(nome, pergunta1, pergunta2, pergunta3, pergunta4, pergunta5));
+			
+			novo = JOptionPane.showConfirmDialog(null, "Deseja inserir dados pra outro inover?");
+		}
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter(caminhoAquivo))) {
+
+			for (Inover inover : lista) {
+				bw.write(inover.getNome() + " = " + String.valueOf(inover.getPergunta1()) + ", " + String.valueOf(inover.getPergunta2())
+				+ ", " + String.valueOf(inover.getPergunta3()) + ", " + String.valueOf(inover.getPergunta4()) 
+				+ ", " + String.valueOf(inover.getPergunta5()));
+				bw.newLine();
+			}
+
+			System.out.println(caminhoAquivo + " CREATED!");
+			
+		} 
+		catch (IOException e) {
+			System.out.println("Error writing file: " + e.getMessage());
+		}
+	
+		
+	}
+
+}
---
+++ src/entities/Inover.java
@@ -0,0 +1,81 @@
+package entities;
+
+public class Inover {
+
+	private String nome;
+	private Integer pergunta1;
+	private Integer pergunta2;
+	private Integer pergunta3;
+	private Integer pergunta4;
+	private Integer pergunta5;
+	
+	public Inover() {
+	}
+
+	public Inover(String nome, int pergunta1, int pergunta2, int pergunta3, int pergunta4, int pergunta5) {
+		this.nome = nome;
+		this.pergunta1 = pergunta1;
+		this.pergunta2 = pergunta2;
+		this.pergunta3 = pergunta3;
+		this.pergunta4 = pergunta4;
+		this.pergunta5 = pergunta5;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getPergunta1() {
+		return pergunta1;
+	}
+
+	public void setPergunta1(int pergunta1) {
+		this.pergunta1 = pergunta1;
+	}
+
+	public int getPergunta2() {
+		return pergunta2;
+	}
+
+	public void setPergunta2(int pergunta2) {
+		this.pergunta2 = pergunta2;
+	}
+
+	public int getPergunta3() {
+		return pergunta3;
+	}
+
+	public void setPergunta3(int pergunta3) {
+		this.pergunta3 = pergunta3;
+	}
+
+	public int getPergunta4() {
+		return pergunta4;
+	}
+
+	public void setPergunta4(int pergunta4) {
+		this.pergunta4 = pergunta4;
+	}
+
+	public int getPergunta5() {
+		return pergunta5;
+	}
+
+	public void setPergunta5(int pergunta5) {
+		this.pergunta5 = pergunta5;
+	}
+
+	@Override
+	public String toString() {
+		return "Inover [nome=" + nome + ", pergunta1=" + pergunta1 + ", pergunta2=" + pergunta2 + ", pergunta3="
+				+ pergunta3 + ", pergunta4=" + pergunta4 + ", pergunta5=" + pergunta5 + "]";
+	}
+	
+	public int mediaFelicidade(int pergunta1, int pergunta2, int pergunta3, int pergunta4, int pergunta5 ) {
+		return (pergunta1 + pergunta2 + pergunta3 + pergunta4 + pergunta5)/5;
+	}
+}
